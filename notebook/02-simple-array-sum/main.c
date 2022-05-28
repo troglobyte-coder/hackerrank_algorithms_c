4aabd9fdee6ad23056d1f5c6c6cb50dc82f283b3
Michael Gene Brockus <55331536+michael-gene-brockus@users.noreply.github.com>
2022-05-27 20:33:07 -0700

    Create main.c

---
+++ notebook/02-simple-array-sum/main.c
@@ -0,0 +1,249 @@
+//
+// HackerRank folio
+// solved by Michael Gene Brockus
+//
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+//
+// Should add the values from the vector to
+// the summary and return the value of sum.
+//
+// arg-list:
+//  -> ar: the vector being passed in
+//
+int simpleArraySum(int ar_count, int* ar)
+{
+    int sum = 0;
+    for (int iter = 0; iter < ar_count; iter++)
+    {
+        sum += ar[iter];
+    } // end for
+    return sum;
+} // end of func
+
+//
+// main is where all C++ programs start
+//
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int ar_count = parse_int(ltrim(rtrim(readline())));
+
+    char** ar_temp = split_string(rtrim(readline()));
+
+    int* ar = malloc(ar_count * sizeof(int));
+
+    for (int i = 0; i < ar_count; i++)
+    {
+        int ar_item = parse_int(*(ar_temp + i));
+
+        *(ar + i) = ar_item;
+    } // end for
+
+    int result = simpleArraySum(ar_count, ar);
+
+    fprintf(fptr, "%d\n", result);
+
+    fclose(fptr);
+
+    return 0;
+} // end of func
+
+//
+// Should readlines of data from the text
+// file into the program for use.
+//
+// arg-list:
+//  -> N/A:
+//
+char* readline()
+{
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true)
+    {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line)
+        {
+            break;
+        } // end if
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n')
+        {
+            break;
+        } // end if
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data)
+        {
+            data = '\0';
+
+            break;
+        } // end if
+
+    } // end while
+
+    if (data[data_length - 1] == '\n')
+    {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data)
+        {
+            data = '\0';
+        } // end if
+
+    } // end if
+    else
+    {
+        data = realloc(data, data_length + 1);
+
+        if (!data)
+        {
+            data = '\0';
+        } // end if
+        else
+        {
+            data[data_length] = '\0';
+        } // end else
+
+    } // end else
+
+    return data;
+} // end of func
+
+//
+// Should trim the data read in from the test case
+// file being used in this problem.
+//
+// arg-list:
+//  -> str: string refrence being passed in for left trim
+//
+char* ltrim(char* str)
+{
+    if (!str)
+    {
+        return '\0';
+    } // end if
+
+    if (!*str)
+    {
+        return str;
+    } // end if
+
+    while (*str != '\0' && isspace(*str))
+    {
+        str++;
+    } // end while
+
+    return str;
+} // end of func
+
+//
+// Should trim the data read in from the test case
+// file being used in this problem.
+//
+// arg-list:
+//  -> str: string refrence being passed in for right trim
+//
+char* rtrim(char* str)
+{
+    if (!str)
+    {
+        return '\0';
+    } // end if
+
+    if (!*str)
+    {
+        return str;
+    } // end if
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end))
+    {
+        end--;
+    } // end while
+
+    *(end + 1) = '\0';
+
+    return str;
+} // end of func
+
+//
+// Should split the data into tokens so they can be used
+// in the problem.
+//
+// arg-list:
+//  -> str: string being split
+//
+char** split_string(char* str)
+{
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token)
+    {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits)
+        {
+            return splits;
+        } // end if
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    } // end while
+
+    return splits;
+} // end of func
+
+//
+// Should parse integer values from the data file.
+//
+// arg-list:
+//  -> str: string being parsed into integers
+//
+int parse_int(char* str)
+{
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0')
+    {
+        exit(EXIT_FAILURE);
+    } // end if
+
+    return value;
+} // end of func
